64abe75563e2ec0d317eec852e57792fc200e539
zsxfa <1844577897@qq.com>
2020-12-26 16:33:10 +0800

    更新本地库

--- src/main/java/com/jachin/git/HelloGit.java
+++ src/main/java/com/jachin/git/HelloGit.java
@@ -6,7 +6,7 @@ public class HelloGit {
 		System.out.println("Hello Git");
 		System.out.println("Hello Git");
 		System.out.println("Hello Git");
-		System.out.println("更新1");
+		System.out.println("更新1"); 
 		System.out.println("主干添加");
 		System.out.println("分支添加");
 	}
