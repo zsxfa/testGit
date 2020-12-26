d852cf97643be2cd646fcf238f8b20a9f3f59c52
zsxfa <66770825+zsxfa@users.noreply.github.com>
2020-12-26 16:21:42 +0800

    Update HelloGit.java

--- src/main/java/com/jachin/git/HelloGit.java
+++ src/main/java/com/jachin/git/HelloGit.java
@@ -5,13 +5,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("更新1");
-<<<<<<< HEAD
-		System.out.println("创建分支");
 		System.out.println("主干添加");
-=======
-		System.out.println("创建分支"); 
 		System.out.println("分支添加");
->>>>>>> refs/heads/dev
 	}
 
 }
